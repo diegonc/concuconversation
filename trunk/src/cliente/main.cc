fcff225f7a3839dc91f32b3407f97315ecfe76b1
dnietoc <dnietoc@90fef904-7202-0c73-447b-e267bf652d28>
2011-04-30 18:33:28 +0000

    Mensaje Join.

--- trunk/src/cliente/main.cc
+++ trunk/src/cliente/main.cc
@@ -1,5 +1,6 @@
 #include <SalonRemoto.h>
 #include <Message.h>
+#include <JoinMessage.h>
 
 class MyMessage : public Message
 {
@@ -15,5 +16,7 @@ int main (int argc, char **argv)
 	SalonRemoto salon(std::string("test"));
 
 	salon.post (MyMessage());
+	salon.post (JoinMessage ("usuario"));
+
 	return 0;
 }

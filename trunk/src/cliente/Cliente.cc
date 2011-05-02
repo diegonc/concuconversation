12be158cf8f4c1f018c04bcf4ecee4c485d53699
dnietoc <dnietoc@90fef904-7202-0c73-447b-e267bf652d28>
2011-05-02 22:05:55 +0000

    ConsoleManager que administra curses.

--- trunk/src/cliente/Cliente.cc
+++ trunk/src/cliente/Cliente.cc
@@ -5,77 +5,26 @@
 
 #include <Cliente.h>
 
-Cliente::Cliente () : input_size (4)
+Cliente::Cliente (int arg, char **argv)
+	: console (*this)
 {
-	/* Inicializacion de curses. */
-	initscr ();
-	cbreak ();
-	noecho ();
 
-	/* 0          0,COLS
-	 * +----------+
-	 * |          |
-	 * |          |
-	 * |          |
-	 * |          |
-	 * +----------+ LINES - input_size
-	 * |          |
-	 * |          |
-	 * +----------+
-	 *            LINES,COLS
-	 */
-	msg = newwin (LINES - input_size, COLS, 0, 0);
-	scrollok (msg, TRUE);
-
-	input = newwin (input_size, COLS, LINES - input_size, 0);
-	keypad (input, TRUE);
 }
 
 Cliente::~Cliente ()
 {
-	delwin (msg);
-	delwin (input);
-	endwin ();
+
 }
 
 
 void Cliente::run ()
 {
-	bool running = true;
-	int c;
-	bool escaped = false;
-
-	while (running) {
-		c = wgetch (input);
-
-		wprintw (msg, "key: %s (%d)\n", keyname (c), c);
-		
-		if (escaped) {
-			if (c == 'q')
-				break;
-			escaped = false;
-		} if (isprint (c)) {
-			input_buffer.push_back (c);
-			waddch (input, c);
-		} else if (c == KEY_BACKSPACE) {
-			if (input_buffer.size () > 0)
-				input_buffer.pop_back ();
-			waddch (input, '\b');
-			wdelch (input);
-		} else if (c == KEY_ENTER || c == '\n') {
-			input_buffer.push_back ('\n');
-
-			std::string text (input_buffer.begin(), input_buffer.end());
-			waddstr (msg, text.c_str ());
-			input_buffer.clear ();
-			werase (input);
-		} else if (c == 27)
-			escaped = true;
-
-		wnoutrefresh (msg);
-		wnoutrefresh (input);
-		doupdate ();
-	}
+	console.run ();
 }
 
+void Cliente::onInputLine (const std::string& text)
+{
+	// salon.post (TextMessage (text))
+	console.append (text);
+}
 
--- trunk/src/lib/chat/Salon.h
+++ trunk/src/lib/chat/Salon.h
@@ -1,14 +1,19 @@
 #ifndef SALON_H
 #define SALON_H
 
-#include <chat/Message.h>
+#include <chat/TextMessage.h>
+#include <chat/Usuario.h>
 
 class Salon
 {
 	public:
 		virtual ~Salon();
 
-		virtual void post (const Message& msg) = 0;
+		virtual void join (Usuario& u) = 0;
+		virtual void quit (Usuario& u) = 0;
+
+		virtual void post (const TextMessage& msg) = 0;
+
 };
 
 #endif

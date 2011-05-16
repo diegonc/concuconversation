e51bf8dec75a64d255ff49b53bd40e0808a7c29f
dnietoc <dnietoc@90fef904-7202-0c73-447b-e267bf652d28>
2011-05-16 01:05:49 +0000

    log4cxx en salon.

--- src/salon/main.cc
+++ src/salon/main.cc
@@ -1,14 +1,53 @@
 #include <stdexcept>
 #include <iostream>
+#include <typeinfo>
 
 #include <ArgParser.h>
 #include <SalonServer.h>
 
+#include <log4cxx/consoleappender.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/logmanager.h>
+#include <log4cxx/patternlayout.h>
+#include <log4cxx/helpers/pool.h>
+
+static void configure_root_logger (bool debug)
+{
+	log4cxx::LoggerPtr root = log4cxx::Logger::getRootLogger ();
+	log4cxx::ConsoleAppenderPtr appender (new log4cxx::ConsoleAppender ());
+	log4cxx::LogString TTCC_CONVERSION_PATTERN (
+		LOG4CXX_STR("%r %p %c - %m%n"));
+	log4cxx::LayoutPtr layout (new log4cxx::PatternLayout (
+			TTCC_CONVERSION_PATTERN));
+
+	if (debug)
+		root->setLevel (log4cxx::Level::getDebug ());
+	else
+		root->setLevel (log4cxx::Level::getError ());
+
+	appender->setLayout (layout);
+
+	log4cxx::helpers::Pool pool;
+	appender->activateOptions (pool);
+
+	root->addAppender (appender);
+
+	log4cxx::LogManager::getLoggerRepository()->setConfigured (true);
+	LOG4CXX_DEBUG (root, "log4cxx inicializado.");
+}
+
 int main (int argc, char **argv)
 {
 	try {
+		ArgParser args(argc, argv);
+
+		configure_root_logger (args.debug ());
+
 		SalonServer server (ArgParser (argc, argv));
 		server.run ();
+	} catch (std::exception& e) {
+		std::cout << typeid (e).name () << ": " << e.what () << std::endl;
+		return 1;
 	} catch (...) {
 		/* correr destructores por favor. */
 		return 1;

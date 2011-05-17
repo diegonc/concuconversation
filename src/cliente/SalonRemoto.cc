a73d20c86bf73e6c3c0c04ff443dcd16a1890934
dnietoc <dnietoc@90fef904-7202-0c73-447b-e267bf652d28>
2011-05-17 23:37:27 +0000

    Cliente no invoca al salon. Debe lanzarse de antemano.

--- src/cliente/SalonRemoto.cc
+++ src/cliente/SalonRemoto.cc
@@ -28,34 +28,8 @@ SalonRemoto::~SalonRemoto ()
 	delete fifo;
 }
 
-/**
- * Inicia el salón remoto si este no ha sido iniciado.
- */
 FifoOutputStream* SalonRemoto::iniciar (const std::string& salon)
 {
-	int err;
-
-	/*
-	 * Crea el fifo a través del qué se comunicará con el
-	 * salón.
-	 */
-	err = mkfifo (salon.c_str (), 0666);
-	LOG4CXX_DEBUG(logger, "Cree el fifo " << err << " | " << errno);
-	/*
-	 * Si el fifo no existía se inicia el proceso que administrará
-	 * la conversación y que contiene el objeto Salon al que este
-	 * proxy accede.
-	 */
-	if (err == 0) {
-		char *const argv[] = { "salon",
-			const_cast<char *> (salon.c_str ()), NULL };
-		LOG4CXX_DEBUG(logger, "lanzando salon con parametros " << argv);
-		System::spawn ("../salon/salon", argv);
-		/* TODO: limpiar en caso de error. */
-	} else if (err == -1 && errno != EEXIST) {
-		throw SystemErrorException ();
-	}
-
 	/*
 	 * Abre el fifo durmiendo hasta que el salón haya terminado
 	 * su inicialización y abrá su extremo.
